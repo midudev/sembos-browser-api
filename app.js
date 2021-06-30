e3cd6b506d3ab99d596d5f5471b69d01f02ab6cf
Dawid Roch <61207777+RochDawid@users.noreply.github.com>
2021-06-30 15:44:03 +0200

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const https = require('http');
-
-const port = 3001;
-
-https.createServer((req, res) => {
-  res.writeHead(200);
-  res.end("Hi Sembo! This is the server application running :)\n");
-}).listen(port);
